ec07c926296f4201b6847c7c6f4fe2fce150e94c
Tomer Amsalem <tomerams@gmail.com>
2025-09-16 06:52:20 +0300

    change to assignTo

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -521,6 +521,10 @@ export interface ApiLeadLead extends Struct.CollectionTypeSchema {
     draftAndPublish: false;
   };
   attributes: {
+    assignTo: Schema.Attribute.Relation<
+      'manyToOne',
+      'plugin::users-permissions.user'
+    >;
     client: Schema.Attribute.Relation<'manyToOne', 'api::client.client'>;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
@@ -542,10 +546,6 @@ export interface ApiLeadLead extends Struct.CollectionTypeSchema {
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
-    user: Schema.Attribute.Relation<
-      'manyToOne',
-      'plugin::users-permissions.user'
-    >;
     utm: Schema.Attribute.JSON;
   };
 }
